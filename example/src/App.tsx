5936736bffe895a94afd7c23e6015d912ace0114
RogelioLB <rogelio20052011@gmail.com>
2023-04-01 11:40:19 -0700

    Dist

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import './App.css'
 import VideoPlayer from "@rogeliolb/react-v-player"
-import video from "./../public/video.mp4"
 
 function App() {
 
   return (
     <div className="App">
       <div className="container">
-        <VideoPlayer width="100%" height="100%" url={video}/>
+        <VideoPlayer width="100%" height="100%" url="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"/>
       </div>
     </div>
   )
